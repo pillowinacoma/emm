d1ec2917f608ebf7fe8af8ff6497ca4969a12199
Abdelaziz Sbaai <b132011@gmail.com>
2021-12-01 13:53:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
+setup(
+   name='emm',
+   version='1.0',
+   description='Exceptional Model Mining implementation',
+   author='MathynS',
+   packages=['emm'],
+   install_requires=requirements
+)
